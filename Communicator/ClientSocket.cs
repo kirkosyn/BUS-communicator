f0b5f55ac2010876e7ed41b89c51c15708a5fa59
kirkosyn <38506431+kirkosyn@users.noreply.github.com>
2018-11-12 18:28:08 +0100

    Uporządkowany kod

---
+++ Communicator/ClientSocket.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Communicator
+{
+    class ClientSocket
+    {
+        /// <summary>
+        /// Socket clienta
+        /// </summary>
+        private Socket clientSocket;
+        /// <summary>
+        /// Tablica bitów wiadomości
+        /// </summary>
+        private byte[] buffer = new byte[1024];
+        /// <summary>
+        /// Zakończenie gniazdka
+        /// </summary>
+        EndPoint remoteEndPoint;
+        /// <summary>
+        /// Odebrana wiadomość
+        /// </summary>
+        string receivedMessage = "";
+
+        /// <summary>
+        /// Konstruktor socketa clienta
+        /// </summary>
+        /// <param name="ip">IPAddress</param>
+        /// <param name="port">Port</param>
+        public ClientSocket(string ip, int port)
+        {
+            IPAddress ipAddress = IPAddress.Parse(ip);
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port);
+
+            clientSocket = new Socket(ipAddress.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
+            clientSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            clientSocket.Bind(localEndPoint);
+            GeneratePrimeNumber();
+        }
+
+        /// <summary>
+        /// Zwraca socket clienta
+        /// </summary>
+        /// <returns>Socket</returns>
+        public Socket ReturnSocket()
+        {
+            return clientSocket;
+        }
+
+        /// <summary>
+        /// Łączy z socketem
+        /// </summary>
+        /// <param name="toIp">IPAddress</param>
+        /// <param name="toPort">Port</param>
+        public void SocketConnect(string toIp, int toPort)
+        {
+            IPAddress ipAddress = IPAddress.Parse(toIp);
+            remoteEndPoint = new IPEndPoint(ipAddress, toPort);
+            clientSocket.Connect(remoteEndPoint);
+        }
+
+        /// <summary>
+        /// Rozpoczyna odbieranie wiadomości
+        /// </summary>
+        public void Receive()
+        {
+            buffer = new byte[1024];
+            clientSocket.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref remoteEndPoint,
+                new AsyncCallback(MessageCallback), buffer);
+        }
+
+        /// <summary>
+        /// Przy zdarzeniu dostania wiadomości pobiera ją
+        /// </summary>
+        /// <param name="result">zdarzenie</param>
+        public void MessageCallback(IAsyncResult result)
+        {
+            try
+            {
+                byte[] receivedData = new byte[1024];
+                receivedData = (byte[])result.AsyncState;
+                ASCIIEncoding encoding = new ASCIIEncoding();
+                int i;
+
+                if (receivedData.Length > 0)
+                {
+                    i = receivedData.Length - 1;
+                    while (receivedData[i] == 0)
+                    {
+                        --i;
+                        if (i < 0)
+                            break;
+                    }
+                }
+                else
+                    i = 0;
+                
+                byte[] auxtrim = new byte[i + 1];
+                Array.Copy(receivedData, auxtrim, i + 1);
+
+                receivedMessage = encoding.GetString(auxtrim);
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        /// <summary>
+        /// Zamyka socket
+        /// </summary>
+        public void SocketClose()
+        {
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+        }
+
+        /// <summary>
+        /// Wysyłanie wiadomości do socketa
+        /// </summary>
+        /// <param name="data">treść wiadomości</param>
+        public void SendMessage(string data)
+        {
+            ASCIIEncoding enc = new ASCIIEncoding();
+            byte[] sending = new byte[1024];
+            sending = enc.GetBytes(data);
+            clientSocket.Send(sending);
+        }
+
+        /// <summary>
+        /// Zwraca wiadomość, którą socket dostał
+        /// </summary>
+        /// <returns>wiadomość tekstowa</returns>
+        public String ReturnMessage()
+        {
+            string msg = receivedMessage;
+            receivedMessage = "";
+            return msg;
+        }
+
+        /// <summary>
+        /// Generowanie liczby pierwszej z pliku
+        /// </summary>
+        public void GeneratePrimeNumber()
+        {
+            string fileStream = @"primes.txt", line;
+            Random rnd = new Random();
+            int number = rnd.Next(0, 10000) * 2;
+            line = File.ReadLines(fileStream).Skip(number).Take(1).First();
+            int prime = Int32.Parse(line);
+            //System.IO.File.WriteAllText(@"line.txt", line);
+        }
+
+
+        public void Accept()
+        {
+            clientSocket.Listen(200);
+            clientSocket.BeginAccept(AcceptCallback, null);
+        }
+
+        public void SendCallback(IAsyncResult result)
+        {
+            Socket friend = (Socket)result.AsyncState;
+            int bytesSent = friend.EndSend(result);
+        }
+
+        public void ConnectCallback(IAsyncResult result)
+        {
+            clientSocket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, ReceivedCallback, null);
+        }
+
+        public void AcceptCallback(IAsyncResult result)
+        {
+            Socket friend = clientSocket.EndAccept(result);
+            Accept();
+            friend.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, ReceivedCallback, friend);
+        }
+
+        public void ReceivedCallback(IAsyncResult result)
+        {
+            Socket friend = result.AsyncState as Socket;
+            int bufferSize = friend.EndReceive(result);
+            byte[] packet = new byte[bufferSize];
+            Array.Copy(buffer, packet, packet.Length);
+            friend.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, ReceivedCallback, friend);
+
+        }
+
+    }
+}
---
+++ Communicator/MainWindow.xaml.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Communicator
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Socket pomocniczy
+        /// </summary>
+        Socket mySocket;
+        /// <summary>
+        /// Socket clienta
+        /// </summary>
+        ClientSocket client;
+        /// <summary>
+        /// Zakończenie gniazdka
+        /// </summary>
+        EndPoint endRemote;
+        /// <summary>
+        /// Tablica bitów wiadomości
+        /// </summary>
+        byte[] buffer;
+
+        /// <summary>
+        /// Konstruktor okna
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Wysłanie wiadomości po kliknięciu entera
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TextBox_Enter(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                string sending = Message.Text;
+
+                client.SendMessage(sending);
+                lock (List)
+                {
+                    List.Items.Add("You: " + Message.Text);
+                }
+
+                Message.Clear();
+            }
+        }
+
+        /// <summary>
+        /// Utworzenie socketu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SetMySocket_Click(object sender, RoutedEventArgs e)
+        {
+            string myIp;
+            int myPort;
+            myIp = myIP.Text;
+            myPort = Int32.Parse(myPORT.Text);
+            
+            client = new ClientSocket(myIp, myPort);
+            mySocket = client.ReturnSocket();
+            
+            MessageBox.Show("Socket set.");
+           
+        }
+
+        /// <summary>
+        /// Łączenie z socketem
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SocketConnect_Click(object sender, RoutedEventArgs e)
+        {
+            string toIp;
+            int toPort;
+            toPort = Int32.Parse(toPORT.Text);
+            toIp = toIP.Text;
+            endRemote = new IPEndPoint(IPAddress.Parse(toIp), toPort);
+
+            client.SocketConnect(toIp, toPort);
+
+            buffer = new byte[1024];
+            mySocket.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref endRemote,
+               new AsyncCallback(MessageCallback), buffer);
+
+            MessageBox.Show("Connected.");
+        }
+
+        /// <summary>
+        /// Wysyłanie wiadomości i wypisanie do chatu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SendMessage(object sender, RoutedEventArgs e)
+        {
+            string sending = Message.Text;
+
+            lock (List)
+            {
+                List.Items.Add("You: " + Message.Text);
+            }
+            
+            Message.Clear();
+            client.SendMessage(sending);
+        }
+
+        /// <summary>
+        /// Zamyka socket
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SocketDisconnect_Click(object sender, RoutedEventArgs e)
+        {
+            client.SocketClose();
+            MessageBox.Show("Disconnected.");
+        }
+
+        /// <summary>
+        /// Przy zdarzeniu odebrania wiadomości pobiera ją i wypisuje do chatu
+        /// </summary>
+        /// <param name="result">zdarzenie</param>
+        private void MessageCallback(IAsyncResult result)
+        {
+            try
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    client.MessageCallback(result);
+
+                    lock (List)
+                    {
+                        List.Items.Add("Friend: " + client.ReturnMessage());
+                    }
+                    
+                    buffer = new byte[1024];
+                    mySocket.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref endRemote,
+                        new AsyncCallback(MessageCallback), buffer);
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
