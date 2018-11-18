9647cad9db9c509ef701d2ed5739058d7800cba1
kirkosyn <38506431+kirkosyn@users.noreply.github.com>
2018-11-18 15:51:03 +0100

    Klasa hashująca

---
+++ Communicator/HashProgram.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Communicator
+{
+    class HashProgram
+    {
+        public HashProgram()
+        {
+
+        }
+        public byte[] DoHash(string source)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] hash = GetHash(sha256Hash, source);
+                return hash;
+            }
+        }
+
+        public byte[] DoHash(byte[] source)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                HashAlgorithm hashAlgorithm = sha256Hash;
+                byte[] hash = hashAlgorithm.ComputeHash(source);
+                return hash;
+            }
+        }
+
+        private byte[] GetHash(HashAlgorithm hashAlgorithm, string input)
+        {
+            byte[] data = hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
+            return data;
+        }
+
+
+    }
+}
